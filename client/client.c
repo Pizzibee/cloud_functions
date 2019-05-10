625600241b4cd228f0ba0365b1ea1569e5284285
pizzibee <sushilghambir@gmail.com>
2019-05-10 12:18:01 +0200

    client done en gros

--- client/client.c
+++ client/client.c
@@ -12,30 +12,86 @@
 #define MAX_PARA_PROGRAM 50
 
 int pipefd[2];
+char *adr;
+int port;
+
+void sendExecToServer(int programNumber){
+  int sockfd = initSocketClient(adr, port);
+  int commandToSend = -2;
+  int ret;
+  ret = write(sockfd, &commandToSend, sizeof(int));
+  checkNeg(ret, "write client error");
+  ret = write(sockfd, &programNumber, sizeof(int));
+  checkNeg(ret, "write client error");
+  //read response from server
+  int programNumberReceived;
+  int programState;
+  ret = read(sockfd, &programNumberReceived, sizeof(int));
+  checkNeg(ret, "read client error");
+  printf("Id du programme executé : %d\n", programNumberReceived);
+  ret = read(sockfd, &programState, sizeof(int));
+  checkNeg(ret, "read client error");
+  printf("etat prog : %d\n", programState);
+  switch (programState) {
+    case -2:{
+      printf("Le programme %d n'existe pas\n", programNumberReceived);
+      break;
+    }
+    case -1:{
+      printf("Le programme %d ne compile pas\n", programNumberReceived);
+      break;
+    }
+    case -0:{
+      printf("Le programme %d ne s'est pas terminé normalement\n", programNumberReceived);
+      break;
+    }
+    case 1:{
+      printf("Le programme %d s'est terminé normalement\n", programNumberReceived);
+      long executionTime;
+      int returnCode;
+      ret = read(sockfd, &executionTime, sizeof(long));
+      checkNeg(ret, "read client error");
+      printf("Le temps d'exécution du programme : %ld microsecondes\n", executionTime);
+      ret = read(sockfd, &returnCode, sizeof(int));
+      checkNeg(ret, "read client error");
+      printf("Le code de retour du programme : %d\n", returnCode);
+      char buffer[READ_SIZE];
+      int readChar;
+      printf("Afficha à la sortie standard du programme %d :\n", programNumberReceived);
+      while  ((readChar = read(sockfd, buffer, READ_SIZE*sizeof(char))) != 0 ){
+        printf("%s\n", buffer);
+      }
+      break;
+    }
+    default:{
+      printf("etat du prog reçu %d\n", programState);
+      break;
+    }
+  }
+  close(sockfd);
+}
 
 void recurrent_handler(void* pipe0, void* pipe1){
-  printf("Fils 1\n");
   int tab[MAX_PARA_PROGRAM];
   int logicalSize = 0;
-  //read pipe
-  //if from pere then add to tab
-  //if from brother then iterate through tab and send exec commands to server
   int ret;
   ret = close(pipefd[1]);
   checkNeg(ret, "close pipe client error recurrent function");
   while (true) {
     Message messageReceived;
+    //read pipe
     read(pipefd[0], &messageReceived, sizeof(Message));
-    printf("type received : %d\n", messageReceived.type);
-    printf("programNumber received %d\n", messageReceived.programNumber);
+    printf("Type received : %d\n", messageReceived.type);
     if (messageReceived.type == 1){
+      //if from pere then add to tab
+      printf("ProgramNumber received %d\n", messageReceived.programNumber);
       tab[logicalSize] = messageReceived.programNumber;
       logicalSize++;
     }else{
+      //else from brother then iterate through tab and send exec commands to server
       for (int i = 0; i < logicalSize; i++) {
-        printf("%d\n", tab[i]);
+        sendExecToServer(tab[i]);
       }
-      printf("type 2 reccurent function done\n");
     }
   }
   close(pipefd[0]);
@@ -47,7 +103,6 @@ void timer_handler(void* delay){
   int ret;
   ret = close(pipefd[0]);
   checkNeg(ret, "close pipe client error timer");
-  printf("Fils 2 %d\n", delayInt);
   while (true) {
     sleep(delayInt);
     Message message;
@@ -57,14 +112,6 @@ void timer_handler(void* delay){
   close(pipefd[1]);
 }
 
-void sendExecToServer(int sockfd, int programNumber){
-  int commandToSend = -2;
-  int ret;
-  ret = write(sockfd, &commandToSend, sizeof(int));
-  checkNeg(ret, "write client error");
-  ret = write(sockfd, &programNumber, sizeof(int));
-  checkNeg(ret, "write client error");
-}
 
 int main(int argc, char *argv[]) {
   if (argc != 4){
@@ -73,8 +120,8 @@ int main(int argc, char *argv[]) {
   }
 	char input[DEFAULT_SIZE];
 	int ret;
-  char *adr = argv[1];
-  int port = atoi(argv[2]);
+  adr = argv[1];
+  port = atoi(argv[2]);
   int delay = atoi(argv[3]);
   ret = pipe(pipefd);
   checkNeg(ret, "create pipe client error");
@@ -83,23 +130,25 @@ int main(int argc, char *argv[]) {
   ret = close(pipefd[0]);
   checkNeg(ret, "close pipe client error main");
 
-  //-- boucle while
 	while(true){
-    int sockfd = initSocketClient(adr, port);
+    //read on stdin
     printf("Veuillez entrez une commande : \n");
   	ret = read(0, &input, DEFAULT_SIZE);
   	checkNeg(ret, "read client error");
   	input[ret-1] = '\0';
+    //extract command + * @ q
   	char command = input[0];
   	switch (command) {
   		case '+':{
         //send command to server to add program
+        int sockfd = initSocketClient(adr, port);
   			int commandToSend = -1;
+  			int fileNameLength = strlen(input+2);
   			ret = write(sockfd, &commandToSend, sizeof(int));
   			checkNeg(ret, "write client error");
-  			int fileNameLength = strlen(input+2);
   			ret = write(sockfd, &fileNameLength, sizeof(int));
   			checkNeg(ret, "write client error");
+        //extract filename from path entered
   			int fileNameIndex = -1;
   			for (int i = fileNameLength; i >= 0; i--) {
   				if (input[i] == '/'){
@@ -108,13 +157,16 @@ int main(int argc, char *argv[]) {
   				}
   			}
   			if (fileNameIndex == -1){
+          //if only file name entered
   				ret = write(sockfd, input+2, fileNameLength*sizeof(char));
   			}else{
+          //if path entered
   				ret = write(sockfd, input+fileNameIndex, fileNameLength*sizeof(char));
   			}
   			checkNeg(ret, "write client error");
+        //open and reads the file and write its content to socket
   			int fd = open(input+2, O_RDONLY | O_CREAT, 0644);
-  			checkNeg(fd, "file descriptor client error");
+  			checkNeg(fd, "open client error");
   			char buffer[READ_SIZE];
   			int readChar;
   			while  ((readChar = read(fd, buffer, READ_SIZE*sizeof(char))) != 0 ){
@@ -123,23 +175,24 @@ int main(int argc, char *argv[]) {
   			}
   			close(fd);
   			shutdown(sockfd, SHUT_WR);
+        //reads the response from the server and displays it to stdout
   			int programNumber;
-  			ret = read(sockfd, &programNumber, sizeof(int));
-  			checkNeg(ret, "read client programNumber error");
-  			printf("Id du programme ajouté : %d\n", programNumber);
-  			printf("Message d'erreur du compilateur :\n");
         char buffer2[READ_SIZE];
   			int readChar2;
+  			ret = read(sockfd, &programNumber, sizeof(int));
+  			checkNeg(ret, "read client error");
+  			printf("Id du programme ajouté : %d\n", programNumber);
+  			printf("Message d'erreur du compilateur :\n");
   			while  ((readChar2 = read(sockfd, buffer2, READ_SIZE*sizeof(char))) != 0 ){
   				printf("%s\n", buffer2);
   			}
+      	close(sockfd);
   		 	break;
   		}
   		case '@':{
         //send command to server to execute program
         int programNumber = atoi(&input[2]);
-        sendExecToServer(sockfd, programNumber);
-        //read les reponses du serveur
+        sendExecToServer(programNumber);
   			break;
   		}
       case '*':{
@@ -148,18 +201,14 @@ int main(int argc, char *argv[]) {
         Message message;
         message.type = 1;
         message.programNumber = programNumber;
-        printf("bklablabal\n");
         write(pipefd[1], &message, sizeof(Message));
         break;
       }
       case 'q':{
+        exit(1);
         //quit server and free ressources
       }
-  	 	default:
-  			printf("ya pas encore\n");
-	}
-  	close(sockfd);
-}
-  //fin boucle while
+	  }
+  }
   close(pipefd[1]);
 }
